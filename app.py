ddd1c61fe1f7cb49fb5f54ef0867e1a16537f940
ABHISHEK JHA <abhishekjha2468@gmail.com>
2021-10-27 15:41:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,16 +9,16 @@ import math
 #downloading model from my github
 #github id --> https://github.com/abhishekjha2468
 import requests
-url = 'https://github.com/abhishekjha2468/Dont-Overfit/blob/main/LG.pkl?raw=true'
-with open("LG.pkl",'wb') as output_file: output_file.write(requests.get(url).content)
-url = 'https://github.com/abhishekjha2468/Dont-Overfit/blob/main/XGBoost.pkl?raw=true'
-with open("XGBoost.pkl",'wb') as output_file: output_file.write(requests.get(url).content)
-url = 'https://github.com/abhishekjha2468/Dont-Overfit/blob/main/LGBM.pkl?raw=true'
-with open("LGMB.pkl",'wb') as output_file: output_file.write(requests.get(url).content)
-url = 'https://github.com/abhishekjha2468/Dont-Overfit/blob/main/LASSO.pkl?raw=true'
-with open("LASSO.pkl",'wb') as output_file: output_file.write(requests.get(url).content)
-url = 'https://github.com/abhishekjha2468/Dont-Overfit/blob/main/scaler.pkl?raw=true'
-with open("scaler.pkl",'wb') as output_file: output_file.write(requests.get(url).content)
+# url = 'https://github.com/abhishekjha2468/Dont-Overfit/blob/main/LG.pkl?raw=true'
+# with open("LG.pkl",'wb') as output_file: output_file.write(requests.get(url).content)
+# url = 'https://github.com/abhishekjha2468/Dont-Overfit/blob/main/XGBoost.pkl?raw=true'
+# with open("XGBoost.pkl",'wb') as output_file: output_file.write(requests.get(url).content)
+# url = 'https://github.com/abhishekjha2468/Dont-Overfit/blob/main/LGBM.pkl?raw=true'
+# with open("LGMB.pkl",'wb') as output_file: output_file.write(requests.get(url).content)
+# url = 'https://github.com/abhishekjha2468/Dont-Overfit/blob/main/LASSO.pkl?raw=true'
+# with open("LASSO.pkl",'wb') as output_file: output_file.write(requests.get(url).content)
+# url = 'https://github.com/abhishekjha2468/Dont-Overfit/blob/main/scaler.pkl?raw=true'
+# with open("scaler.pkl",'wb') as output_file: output_file.write(requests.get(url).content)
 
 
 #loading model from downloaded pickle file
@@ -48,16 +48,16 @@ def prediction(query):
   pred=lr_pred*0.1 + xgb_pred*0.1 + lgmb_pred*0.1 + lasso_pred*0.7
   return pred
 
-url="https://raw.githubusercontent.com/abhishekjha2468/Dont-Overfit/main/index.html"
-with open("index.html",'wb') as output_file: output_file.write(requests.get(url).content)
+# url="https://raw.githubusercontent.com/abhishekjha2468/Dont-Overfit/main/index.html"
+# with open("index.html",'wb') as output_file: output_file.write(requests.get(url).content)
 
 
 
-import shutil
-try: shutil.rmtree("templates")
-except: pass
-os.mkdir("templates")
-shutil.copy("index.html","templates/index.html")
+# import shutil
+# try: shutil.rmtree("templates")
+# except: pass
+# os.mkdir("templates")
+# shutil.copy("index.html","templates/index.html")
 
 # !pip install flask_ngrok
 
